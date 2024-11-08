a97fbe6528c17ea6f9ed52688204e1cbecc36ea2
prasannakumar50 <babblu293@gmail.com>
2024-11-08 12:45:49 +0530

    fix: update the schema model

--- models/event.models.js
+++ models/event.models.js
@@ -1,47 +1,56 @@
-const mongoose = require("mongoose")
-
+const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-   
-    title:{
-        type:String,
+    title: {
+        type: String,
         required: true
     },
-    description:{
-        type:String,
+    hostedBy: {
+        type: String,
         required: true
     },
-    sessionTimings:[{
-        type:String,
+    description: {
+        type: String,
         required: true
+    },
+    sessionTimings: [{
+        sessionTitle: { type: String, required: true },
+        startTime: { type: String, required: true },
+        endTime: { type: String, required: true }
     }],
-    speakers:[{
-        type:String
+    speakers: [{
+        name: { type: String, required: true },
+        role: { type: String, required: true },
+        profileImageUrl: { type: String }
     }],
     price: {
         type: Number,
-        default: 0      
+        default: 0
     },
-    venue:String,
-    tags:[{
-        type:String
+    venue: String,
+    tags: [{
+        type: String
     }],
-    eventType:{
-        type:String,
-        enum:["Online", "Offline"]
+    eventType: {
+        type: String,
+        enum: ["Online", "Offline"]
     },
     date: {
         type: String,
-        required: true,             
+        required: true
     },
-
     imageUrl: {
-        type: String,  
-        required: true, 
+        type: String,
+        required: true
     },
+    performers: [{
+        name: { type: String, required: true },
+        role: { type: String, required: true },
+        profileImageUrl: { type: String }
+    }]
+    
+}, { timestamps: true });
 
-},{ timestamps: true })
-
-const Event = mongoose.model('Event', eventSchema)
+const Event = mongoose.model('Event', eventSchema);
 
 module.exports = Event;
